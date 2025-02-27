05d4af977ce44682643ca3c7dea9cfc18d8e88ce
Gaurav Kumayan <gauravkumayan91@gmail.com>
2025-02-27 12:16:13 +0530

    added map marker and mappop

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,8 @@
 const Listing = require("../models/listing");
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+
 
 module.exports.index = async (req, res, next) => {
     const allListings = await Listing.find({});
@@ -9,9 +13,9 @@ module.exports.renderNewForm = (req, res) => {
     res.render("listings/new.ejs");
 };
 
-module.exports.showListing     = async (req, res) => {
+module.exports.showListing = async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate({path :"reviews",populate : {path : "author",},}).populate("owner");
+    const listing = await Listing.findById(id).populate({ path: "reviews", populate: { path: "author", }, }).populate("owner");
     if (!listing) {
         req.flash("error", "Listing you requested for does not exist!")
         res.redirect("/listings");
@@ -21,20 +25,30 @@ module.exports.showListing     = async (req, res) => {
 };
 
 
-module.exports.createListing  = async (req, res) => {
-      let url = req.file.path ;
-      let filename  = req.file.filename;
-     console.log(url , "..." , filename);
-      const newlisting = new Listing(req.body.Listing);
-    newlisting.owner = req.user._id;
-     newlisting.image = {url , filename};
-    await newlisting.save();
+module.exports.createListing = async (req, res) => {
+     let response =  await geocodingClient.forwardGeocode({
+        query: req.body.Listing.location,
+        limit: 1,
+      })
+        .send()
     
+
+    let url = req.file.path;
+    let filename = req.file.filename;
+    console.log(url, "...", filename);
+    const newlisting = new Listing(req.body.Listing);
+    newlisting.owner = req.user._id;
+    newlisting.image = { url, filename };
+
+    newlisting.geometry = response.body.features[0].geometry;
+    
+        let saved=   await newlisting.save();
+      console.log(saved);
     req.flash("success", "New Listing Created");
     res.redirect("/listings");
 };
 
-module.exports.renderEditForm   = async (req, res) => {
+module.exports.renderEditForm = async (req, res) => {
     let { id } = req.params;
     console.log(id);
 
@@ -43,13 +57,21 @@ module.exports.renderEditForm   = async (req, res) => {
         req.flash("error", "Listing you requested for does not exist!")
         res.redirect("/listings");
     }
-    res.render("listings/edit.ejs", { listing });
+    let orignalImageUrl =  listing.image.url;
+     orignalImageUrl =  orignalImageUrl.replace("/upload" , "/upload/w_250");
+    res.render("listings/edit.ejs", { listing , orignalImageUrl });
 };
 
 module.exports.updateListing = async (req, res) => {
 
     const { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.Listing });
+    let listing = await Listing.findByIdAndUpdate(id, { ...req.body.Listing });
+    if (typeof req.file !== "undefined") {
+        let url = req.file.path;
+        let filename = req.file.filename;
+        listing.image = { url, filename };
+       await listing.save();
+    }
     req.flash("success", "Listing Edited");
     res.redirect(`/listings/${id}`);
 

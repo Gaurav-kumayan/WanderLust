e7c9eec3f2e34584c54e8bbfc2300747664e659e
Gaurav <gauravkumayan91@gmail.com>
2025-02-28 15:15:00 +0530

    added Atlus and mongo session store

--- app.js
+++ app.js
@@ -4,19 +4,35 @@ if(process.env.NODE_ENV != "production"){
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+const dbUrl = process.env.ATLASDB_URL;
+
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
+const store = MongoStore.create({
+    mongoUrl : dbUrl,
+    crypto  : {
+      secret :  process.env.SECRET,
+    },
+    touchAfter : 24 *3600 ,
+});
+
+store.on("error" , () =>{
+    console.log("ERROR IN MONGO SESSION STORE");
+})
+
 const sessionOptions = {
-    secret: "myseacretcode",
+     store,
+    secret:  process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -25,6 +41,7 @@ const sessionOptions = {
         httpOnly: true
     },
 };
+
 //use before routes
 app.use(session(sessionOptions));
 app.use(flash());
@@ -56,7 +73,7 @@ main().then(() => {
 }).catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
